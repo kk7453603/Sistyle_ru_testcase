34d6db4df9fd970d3c15a5ae2a56c1b51893c6a5
Kiril_K <kk7453603@gmail.com>
2024-09-04 22:56:47 +0300

    main function code removed

--- cmd/main.go
+++ cmd/main.go
@@ -1,12 +1,5 @@
 package main
 
 func main() {
-	/*
-		programList, err := utils.ReadProgramListFromFile("cmd/input_test.json")
-		if err != nil {
-			panic(err)
-		}
-		log.Println(programList)
-		utils.WriteProgramDataToFile("cmd/output_test.json", programList)
-	*/
+
 }
