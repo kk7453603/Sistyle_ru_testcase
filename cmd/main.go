d1f72e42ebb7eaa580fec44bd4b41136979bd602
Kiril_K <kk7453603@gmail.com>
2024-09-04 22:28:20 +0300

    main func commented

--- cmd/main.go
+++ cmd/main.go
@@ -1,15 +1,12 @@
 package main
 
-import (
-	"log"
-	"test/pkg/utils"
-)
-
 func main() {
-	programList, err := utils.ReadProgramDataFromFile("cmd/input_test.json")
-	if err != nil {
-		panic(err)
-	}
-	log.Println(programList)
-	utils.WriteProgramDataToFile("cmd/output_test.json", programList)
+	/*
+		programList, err := utils.ReadProgramListFromFile("cmd/input_test.json")
+		if err != nil {
+			panic(err)
+		}
+		log.Println(programList)
+		utils.WriteProgramDataToFile("cmd/output_test.json", programList)
+	*/
 }
